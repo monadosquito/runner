35e9be9dbcc157016c2b6940e96a67d35b01af6d
monadosquito <monadosquito@gmail.com>
2023-12-22 19:09:13 +0300

    feat(core,driver): add WWW custom configuring

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE CPP #-}
 
 
 import Core.Port.Driver
@@ -11,11 +12,29 @@ import Data.Proxy
 
 import Core.Configuration.Configuration
 import Core.Port.Environment
+
 import qualified Driver.Environment.Www as Env
 
 
+#if defined(WWW) && !defined(__GHCJS__)
+import Language.Javascript.JSaddle
+import qualified Language.Javascript.JSaddle.Warp as Jsaddle
+#endif
 main :: IO ()
-main = do
+main = runApp $ do
     prefs <- getPreferences (Proxy @Env.Www) $ Proxy @Aeson
-    let conf = Configuration prefs defaultOptions 
-    runReaderT (run (Proxy @Driver.Www) $ Proxy @Aeson) conf
+    let conf = Configuration prefs defaultOptions
+    liftIO $ runReaderT (run (Proxy @Driver.Www) $ Proxy @Aeson) conf
+
+#if defined(CONS)
+runApp :: IO () -> IO ()
+runApp = id
+#elif defined(WWW)
+#ifdef __GHCJS__
+runApp :: IO () -> IO ()
+runApp = id
+#else
+runApp :: JSM () -> IO ()
+runApp f = Jsaddle.debugOr 8000 (f >> syncPoint) Jsaddle.jsaddleApp
+#endif
+#endif
